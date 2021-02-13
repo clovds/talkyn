1c69588ba3df582ad77b036aac8488310040f5bc
Wido Wicaksono <widowicak@gmail.com>
2021-02-13 21:01:50 +0700

    loginPage update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.render(
-	<React.Fragment>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.Fragment>,
-	document.getElementById("root")
+  <React.Fragment>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.Fragment>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
